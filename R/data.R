a4207509c6107e7bca12bf252ff59ab166cc4af1
Carlos Cinelli <carloslkcinelli@gmail.com>
2024-05-16 08:10:20 -0700

    data documentation

--- R/data.R
+++ R/data.R
@@ -52,5 +52,7 @@
 #'
 #' Card, D. "Using Geographic Variation in College Proximity to Estimate the Return to Schooling". In L.N. Christofides, E.K. Grant, and R. Swidinsky, editors, Aspects of Labor Market Behaviour: Essays in Honour of John Vanderkamp. Toronto: University of Toronto Press, 1995.
 #'
-#' @examples  head(card)
+#' @examples
+#' data('card')
+#' head(card)
 "card"
